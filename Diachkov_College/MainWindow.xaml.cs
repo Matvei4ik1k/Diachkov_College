3bdee5682f9913f85238c33b494e8059e6068d90
vboxuser <vboxuser@WIN10>
2025-10-07 13:21:17 +0300

    Добавьте файлы проекта.

---
+++ Diachkov_College/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Diachkov_College
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddGroupBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddActivityBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void JournalBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Report1Btn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Report2Btn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
