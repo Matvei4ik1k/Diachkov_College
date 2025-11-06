4612b3b1e8d6fda10417920164729f9dcaeeb4fb
Matvei4ik1k <Matvei4ik1k@WIN10>
2025-11-06 14:38:00 +0300

    ...

--- Diachkov_College/Pages/JournalPage.xaml.cs
+++ Diachkov_College/Pages/JournalPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Diachkov_College.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,6 +25,39 @@ namespace Diachkov_College.Pages
         {
             InitializeComponent();
             JornalLv.ItemsSource = App.context.Journal.ToList();
+
+            SpecialCmb.SelectedValuePath= "Id";
+            SpecialCmb.DisplayMemberPath = "Name";
+            SpecialCmb.ItemsSource = App.context.Spesial.ToList();
+
+            DirectionCmb.SelectedValuePath = "Id";
+            DirectionCmb.DisplayMemberPath = "Name";
+            DirectionCmb.ItemsSource = App.context.Direction.ToList();
+
+            GroupCmb.SelectedValuePath = "Id";
+            GroupCmb.DisplayMemberPath = "Name";
+            GroupCmb.ItemsSource = App.context.Group.ToList();
+
+            ActivityCmb.SelectedValuePath = "Id";
+            ActivityCmb.DisplayMemberPath = "Name";
+            ActivityCmb.ItemsSource = App.context.Activity.ToList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(SpecialCmb.Text)&& string.IsNullOrEmpty(DirectionCmb.Text) && string.IsNullOrEmpty(GroupCmb.Text) && string.IsNullOrEmpty(ActivityCmb.Text)&& string.IsNullOrEmpty(MarkTb.Text))
+            
+            {
+                MessageBox.Show("Заполните все поля!");
+            }
+
+            else
+            {
+                Journal journal = new Journal()
+                {
+                    DateEvent = (DateTime)DateEventDp
+                }
+            }
         }
     }
 }
